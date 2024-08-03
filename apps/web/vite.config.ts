cbf97a7f44e06b44b1b08aa08e5315c4b5a11fc8
shunsei <khan.sufyan.cs@gmail.com>
2024-08-03 19:58:29 +0300

    chore: set up pwa lib and icons

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import { lingui } from "@lingui/vite-plugin";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
@@ -9,6 +10,28 @@ export default defineConfig({
     outDir: "dist",
   },
   plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Bahar",
+        short_name: "Bahar",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
     react({
       plugins: [["@lingui/swc-plugin", {}]],
     }),
