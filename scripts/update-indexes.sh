8cbe7584bbcb663bd01807c366040c7adefdf4a0
shunsei <khan.sufyan.cs@gmail.com>
2024-08-11 12:36:17 +0300

    chore: add tags to filterable attributes

--- scripts/update-indexes.sh
+++ scripts/update-indexes.sh
@@ -3,11 +3,11 @@
 set -e
 
 if [ -z "$MEILISEARCH_HOST" ] || [ -z "$MEILISEARCH_MASTER_KEY" ]; then
-	echo "Please set the MEILISEARCH_HOST and MEILISEARCH_MASTER_KEY environment variables."
-	exit 1
+  echo "Please set the MEILISEARCH_HOST and MEILISEARCH_MASTER_KEY environment variables."
+  exit 1
 fi
 
-FILTERABLE_ATTRIBUTES='["flashcard.due_timestamp"]'
+FILTERABLE_ATTRIBUTES='["flashcard.due_timestamp", "tags"]'
 
 echo "Updating indexes for all users..."
 
@@ -18,20 +18,20 @@ echo "Found indexes: "
 echo [$INDEXES]
 
 for index in $INDEXES; do
-	echo "Updating filterable attributes for index: $index"
-	# Update filterable attributes
-	response=$(curl \
-		-X PUT "$MEILISEARCH_HOST/indexes/$index/settings/filterable-attributes" \
-		-H "Authorization: Bearer $MEILISEARCH_MASTER_KEY" \
-		-H "Content-Type: application/json" \
-		-s \
-		--data-binary "$FILTERABLE_ATTRIBUTES")
+  echo "Updating filterable attributes for index: $index"
+  # Update filterable attributes
+  response=$(curl \
+    -X PUT "$MEILISEARCH_HOST/indexes/$index/settings/filterable-attributes" \
+    -H "Authorization: Bearer $MEILISEARCH_MASTER_KEY" \
+    -H "Content-Type: application/json" \
+    -s \
+    --data-binary "$FILTERABLE_ATTRIBUTES")
 
-	if [[ $response =~ "code" && $response =~ "type" ]]; then
-		echo $response | jq
-		echo "Error updating filterable attributes for index: $index"
-		exit 1
-	fi
+  if [[ $response =~ "code" && $response =~ "type" ]]; then
+    echo $response | jq
+    echo "Error updating filterable attributes for index: $index"
+    exit 1
+  fi
 done
 
 echo "Successfully updated indexes for all users."
