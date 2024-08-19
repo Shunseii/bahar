ecd9ead6347fd3387b52ed37395a6d63ed06724a
shunsei <khan.sufyan.cs@gmail.com>
2024-08-19 16:16:41 +0300

    chore: automatically refresh users app on update

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,10 @@
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 import "./index.css";
+
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
