f506265527651854495af0bb5270b8dbc30d46d9
sufyan <khan.sufyan.cs@gmail.com>
2024-12-20 16:57:08 -0500

    fix: respect redirect url param when using github sso

--- apps/web/src/components/GithubLoginButton.tsx
+++ apps/web/src/components/GithubLoginButton.tsx
@@ -2,16 +2,23 @@ import { Button } from "./ui/button";
 import { GithubLogo } from "./ui/icons";
 import { FC, ReactNode } from "react";
 import { authClient } from "@/lib/auth-client";
+import { Route } from "@/routes/_layout/login/route";
 
 export const GithubLoginButton: FC<{ children: ReactNode }> = ({
   children,
 }) => {
+  const { redirect } = Route.useSearch();
+  const redirectUrl = redirect
+    ? `${window.location.origin}${redirect}`
+    : undefined;
+
   return (
     <Button
       className="bg-black hover:bg-black/85 dark:hover:bg-white/85 dark:bg-white flex items-center"
       onClick={async () => {
         await authClient.signIn.social({
           provider: "github",
+          callbackURL: redirectUrl,
         });
       }}
     >
