185d77ce4bdddef975fd229677d1501782f98059
sufyan <khan.sufyan.cs@gmail.com>
2025-11-29 22:17:57 -0500

    feat: add mockups to landing page

--- apps/marketing/.astro/content-modules.mjs
+++
@@ -1,5 +0,0 @@
-
-export default new Map([
-["src/content/blog/en/welcome-to-bahar.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fen%2Fwelcome-to-bahar.mdx&astroContentModuleFlag=true")],
-["src/content/blog/ar/welcome-to-bahar.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Far%2Fwelcome-to-bahar.mdx&astroContentModuleFlag=true")]]);
-		
